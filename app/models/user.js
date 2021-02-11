5cd27ab2e40e109f78bd5c080c6e025ae4c7f305
Diego Meneses <diego.meneses@wolox.co>
2021-02-11 09:45:53 -0500

    add some contrainst to user model, add tablename and same rules to migrations

--- app/models/user.js
+++ app/models/user.js
@@ -1,14 +1,29 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
-    'user',
+    'User',
     {
-      firstName: DataTypes.STRING,
-      lastName: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
     },
     {
-      underscored: true
+      underscored: true,
+      tableName: 'users',
+      timestamps: true
     }
   );
   return User;
