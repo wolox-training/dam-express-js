260b8f0d29a51169c6eec89a99d0f8f9136c103b
Diego Meneses <diego.meneses@wolox.co>
2021-02-15 16:04:15 -0500

    swagger tocs

--- documentation/schemas/user.js
+++ documentation/schemas/user.js
@@ -3,25 +3,63 @@ module.exports = {
     type: 'integer',
     example: 7
   },
-  username: {
+  userFirstName: {
     type: 'string',
     example: 'tom99'
   },
   userEmail: {
     type: 'string',
-    example: 'tom.engels@wolox.com.ar'
+    example: 'tom.engels@wolox.ar'
+  },
+  userLastName: {
+    type: 'string',
+    example: 'crus'
+  },
+  userPassword: {
+    type: 'string',
+    example: '12345678'
+  },
+  date: {
+    type: 'string',
+    example: '2021-02-15T20:51:32.749Z'
   },
   User: {
+    type: 'object',
+    properties: {
+      first_name: {
+        $ref: '#/components/schemas/userFirstName'
+      },
+      password: {
+        $ref: '#/components/schemas/userPassword'
+      },
+      last_name: {
+        $ref: '#/components/schemas/userLastName'
+      },
+      email: {
+        $ref: '#/components/schemas/userEmail'
+      }
+    }
+  },
+  UserResponse: {
     type: 'object',
     properties: {
       id: {
         $ref: '#/components/schemas/userId'
       },
-      username: {
-        $ref: '#/components/schemas/username'
+      first_name: {
+        $ref: '#/components/schemas/userFirstName'
+      },
+      last_name: {
+        $ref: '#/components/schemas/userLastName'
       },
       email: {
         $ref: '#/components/schemas/userEmail'
+      },
+      created_at: {
+        $ref: '#/components/schemas/date'
+      },
+      updated_at: {
+        $ref: '#/components/schemas/date'
       }
     }
   },
